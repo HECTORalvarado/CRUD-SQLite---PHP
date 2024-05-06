db399ec27a8f4f4fde19fd091902d6e76e1e232d
Hector Alvarado <hectoralexanderalvarado@gmail.com>
2024-05-05 18:02:48 -0600

    funciones del crud completado

--- app/model/notasModel.php
+++ app/model/notasModel.php
@@ -23,4 +23,51 @@ class NotasModel
 
 		return $results;
 	}
+
+	public function getNotaByID($idNota)
+	{
+		$query = "select * from Notas where id_nota = $idNota";
+
+		$statement = $this->PDO->prepare($query);
+
+		$statement->execute();
+
+		$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+		return $results;
+	}
+
+	public function addNota($tituloNota, $contenidoNota)
+	{
+		$query = "INSERT INTO Notas ( titulo_nota, contenido_nota) VALUES ('$tituloNota','$contenidoNota')";
+
+		$statement = $this->PDO->prepare($query);
+
+		$statement->execute();
+
+		printf("hea");
+	}
+
+	public function editNota($idNota, $tituloNota, $contenidoNota)
+	{
+		$query = "UPDATE Notas
+		SET id_nota = $idNota,
+			titulo_nota = '$tituloNota',
+			contenido_nota = '$contenidoNota'
+	  	WHERE id_nota = $idNota";
+
+		$statement = $this->PDO->prepare($query);
+
+		$statement->execute();
+	}
+
+	public function deleteNota($idNota)
+	{
+
+		$query = "delete from Notas where id_nota = $idNota";
+
+		$statement = $this->PDO->prepare($query);
+
+		$statement->execute();
+	}
 }
